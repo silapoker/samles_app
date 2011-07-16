1c0f5b17d687467f9f46d511909eb3b9250da5f6
Dmitry <silapoker@mail.ru>
2011-07-16 22:45:33 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamlesApp::Application.config.secret_token = '6ba8bcc888dc1e81131b562c8d559e1364d2f943ead276687b83734c643d67381113d604f15e84630fd42496722356ab03b939653b87d95197d5e4de9f5c2272'
